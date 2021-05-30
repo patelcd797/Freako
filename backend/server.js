9f3d4a41a59793fa41250e0832d536b0c27c6d6b
Chintan Ilasariya <35325230+patelcd797@users.noreply.github.com>
2021-05-30 16:50:01 +0530

    chore: basic setup for backend

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express') 
+const PORT = 1224
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+const app = express()
+const db = 'mongodb+srv://patelcd797:Patelcd@797@cluster0.tcxn0.mongodb.net/data?retryWrites=true&w=majority'
+mongoose
+  .connect(
+    db,
+    {useNewUrlParser: true, useUnifiedTopology: true }
+  )
+
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+    );
+    next();
+  });
+
+app.listen(PORT, ()=>{
+    console.log('server is listening');
+})
+
